7fea419b3b1bb0dcc9b94e92a3913a50c992e0ea
yuranus <77255167+yuranusduke@users.noreply.github.com>
2021-03-28 21:55:57 +0800

    Update SBAttention.py

--- SBAttention.py
+++ SBAttention.py
@@ -21,56 +21,56 @@ def weights_init(layer):
 class SBAttention(t.nn.Module):
 	"""Define SB operation"""
 	"""According to the paper,
-    y = T(x) + lambda * A(x)
-    where T(x) is the trunk and A(x) = Tanh(F(x))
-    F(x) is one simple DNN like in SE, lambda is learnable parameter
-    to trade off between T(x) and A(x).
-    Notice lambda is channle-wise parameter, so it is one vector.
+		y = T(x) + lambda * A(x)
+		where T(x) is the trunk and A(x) = Tanh(F(x))
+		F(x) is one simple DNN like in SE, lambda is learnable parameter
+		to trade off between T(x) and A(x).
+		Notice lambda is channle-wise parameter, so it is one vector.
 	"""
 
 	def __init__(self, num_channels, device, attn_ratio, activation = 'tanh'):
 		"""
         Args :
-             --num_channels: # of input channels
-			 --device: learning device
-			 --attn_ratio: hidden size ratio
-			 --activation: 'tanh' as default
-		"""
-		super(SBAttention, self).__init__()
+     --num_channels: # of input channels
+		 --device: learning device
+		 --attn_ratio: hidden size ratio
+		 --activation: 'tanh' as default
+	"""
+	super(SBAttention, self).__init__()
 
-		self.num_channels = num_channels
-		self.hidden_size = int(attn_ratio * self.num_channels)
+	self.num_channels = num_channels
+	self.hidden_size = int(attn_ratio * self.num_channels)
 
-		# 1. Trunk, we use T(x) = x like in SE
-		# 2. SB attention
-		if activation == 'tanh':
-			ac = t.nn.Tanh()
+	# 1. Trunk, we use T(x) = x like in SE
+	# 2. SB attention
+	if activation == 'tanh':
+		ac = t.nn.Tanh()
 
-		elif activation == 'sigmoid':
-			ac = t.nn.Sigmoid()
+	elif activation == 'sigmoid':
+		ac = t.nn.Sigmoid()
 
-		elif activation == 'relu':
-			ac = t.nn.ReLU(inplace = True)
+	elif activation == 'relu':
+		ac = t.nn.ReLU(inplace = True)
 
-		elif activation == 'softmax':
-			ac = t.nn.Softmax(dim = -1)
+	elif activation == 'softmax':
+		ac = t.nn.Softmax(dim = -1)
 
-		elif activation == 'linear':
-			ac = t.nn.Identity()
+	elif activation == 'linear':
+		ac = t.nn.Identity()
 
-		else:
-			raise Exception('No other activations!')
+	else:
+		raise Exception('No other activations!')
 
-		self.lambd = t.nn.Parameter(t.randn(1, self.num_channels, device = device), requires_grad = True)
-		self.SB = t.nn.Sequential(
-			t.nn.Linear(self.num_channels, self.hidden_size),
-			t.nn.BatchNorm1d(self.hidden_size),
-			t.nn.ReLU(inplace = True),
+	self.lambd = t.nn.Parameter(t.randn(1, self.num_channels, device = device), requires_grad = True)
+	self.SB = t.nn.Sequential(
+		t.nn.Linear(self.num_channels, self.hidden_size),
+		t.nn.BatchNorm1d(self.hidden_size),
+		t.nn.ReLU(inplace = True),
 
-			t.nn.Linear(self.hidden_size, self.num_channels),
-			t.nn.BatchNorm1d(self.num_channels),
-			ac
-		)
+		t.nn.Linear(self.hidden_size, self.num_channels),
+		t.nn.BatchNorm1d(self.num_channels),
+		ac
+	)
 
 	def forward(self, x):
 		# 1. T(x)
